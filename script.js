f324b966e2967a9b8720774588cc7acdf98f7b61
jebronlames <alexuribe001@gmail.com>
2014-06-08 10:57:30 -0600

    Play/Stop functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Sample use of globals */
 var globals = (function(){
    var pm = {};
    pm.colSelected = 0;
@@ -11,17 +12,24 @@ var globals = (function(){
 
 $(document).ready(function(){
 
+  var lightgreen = "#9BD7D5";
+  var green = "#129793";
+  var boxColor = lightgreen;
+  var activeColor = green;
 	var array = [0,1,2,3];
 	var date = new Date().getTime();
 	var flag = true;
 	var a = 0;
 	var b = 1;
+  var timing = 1000;
+  var intervalVar;
 
 	function run() {
 		$("#box" + 0).css({
 			"background-color": "#9BD7D5"
 		});
-		var time = setInterval(function(){ step() },800);
+    flag = false;
+		intervalVar = setInterval(function(){ step() },timing);
 
 		/*
 		var a = 0;
@@ -29,24 +37,17 @@ $(document).ready(function(){
 			"background-color": "red"
 		});
 		*/
-
-		// Stop step machine
-		$("#play").click(function(){
-   	clearInterval(time);
-   	flag = false;
-   	
-  });
 		return 0;
-		
 	};
 
+  /* Helper function for run().  Steps through boxes */
 	function step(){
 		$("#box" + a).css({
-			"background-color": "#129793"
+			"background-color": green
 		});
 
 		$("#box" + b).css({
-			"background-color": "#9BD7D5"
+			"background-color": lightgreen
 		});
 
 
@@ -58,38 +59,44 @@ $(document).ready(function(){
 
 
 
-
+  /* On play button */
 	$("#play").click(function(){
-		a = 0;
-		b = 1;
-   	run();
+    if (flag == true) { 
+		  a = 0;
+		  b = 1;
+   	  run();
+    } else {
+      clearInterval(intervalVar);
+    }
   });
 
   $(".square").click(function(){
    	alert("Click")
   });
 
+  /* first row functions */
   $(".square")
   	.on("mouseenter", function(){
   		$(this).css({
-  			"background-color": "#9BD7D5"
+  			"background-color": lightgreen
   	});
   })
   	.on("mouseleave", function(){
   		$(this).css({
-  			"background-color": "#129793"
+  			"background-color": green
   		});
   	});
 
+    /* Second row functions */
    $(".square1")
   	.on("mouseenter", function(){
   		$(this).css({
-  			"background-color": "#9BD7D5"
+  			"background-color": lightgreen
   	});
   })
   	.on("mouseleave", function(){
   		$(this).css({
-  			"background-color": "#129793"
+  			"background-color": green
   		});
   	});
 
